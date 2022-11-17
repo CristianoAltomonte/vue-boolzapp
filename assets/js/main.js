d4b61e3335b4a633e88b01cbbcf1f5f85adbd5ab
CristianoAltomonte <cristianoaltomonte@hotmail.it>
2022-11-17 12:55:37 +0000

    Milestone 4

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,10 +174,23 @@ var app = new Vue(
 
     methods: {
 
+      /* Milestone 2
+      Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+      visualizzare tutti i messaggi relativi al contatto attivo all’interno 
+      del pannello della conversazione
+      Click sul contatto mostra la conversazione del contatto cliccato */
+
       selectedChat(element, index) {
 
         this.indiceDinamico = index;
+        
       },
+
+      /* Milestone 3
+      Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
+      “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+      Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+      un “ok” come risposta, che apparirà dopo 1 secondo. */
 
       sendMessage() {
 
@@ -210,6 +223,35 @@ var app = new Vue(
 
       },
 
+
+      // Milestone 4
+      // Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo
+      // i contatti il cui nome contiene le lettere inserite 
+      // (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+      searchIn() {
+
+        this.contacts.forEach(element => {
+
+          let nameLowerCase = element.name.toLowerCase();
+
+          if (nameLowerCase.includes(this.searchName)) {
+
+            return element.visible = true;
+
+          } else {
+
+            return element.visible = false;
+
+          }
+
+        });
+      }
+
+
     },
   }
 )
+
+
+
